a502a8a928b0fd68bbe26d5634d836899334109c
maxg <maxg@negativrekord.de>
2017-10-22 00:05:00 +0200

    gaze

--- tobii_console/tobii_console.cpp
+++ tobii_console/tobii_console.cpp
@@ -1,30 +1,57 @@
 #include "stdafx.h"	
 
 #include <tobii/tobii.h>
+#include <tobii/tobii_streams.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	tobii_api_t* api;
-	tobii_error_t error = tobii_api_create(&api, NULL, NULL);
-	assert(error == TOBII_ERROR_NO_ERROR);
+	int main()
+	{
+		tobii_api_t* api;
+		tobii_error_t error = tobii_api_create(&api, NULL, NULL);
+		assert(error == TOBII_ERROR_NO_ERROR);
 
-	tobii_device_t* device;
-	error = tobii_device_create(api, NULL, &device);
-	assert(error == TOBII_ERROR_NO_ERROR);
+		tobii_device_t* device;
+		error = tobii_device_create(api, NULL, &device);
+		assert(error == TOBII_ERROR_NO_ERROR);
 
-	tobii_device_info_t info;
-	error = tobii_get_device_info(device, &info);
-	assert(error == TOBII_ERROR_NO_ERROR);
+		tobii_device_info_t info;
+		error = tobii_get_device_info(device, &info);
+		assert(error == TOBII_ERROR_NO_ERROR);
 
-	printf("Serial number: %s\n", info.serial_number);
+		printf("Serial number: %s\n", info.serial_number);
 
-	error = tobii_device_destroy(device);
-	assert(error == TOBII_ERROR_NO_ERROR);
 
-	error = tobii_api_destroy(api);
-	assert(error == TOBII_ERROR_NO_ERROR);
-	return 0;
-}
+		error = tobii_gaze_point_subscribe(device,
+			[](tobii_gaze_point_t const* gaze_point, void* user_data)
+		{
+			(void)user_data; // Unused parameter
+			if (gaze_point->validity == TOBII_VALIDITY_VALID)
+				printf ("Gaze point: %g %g %g\n", gaze_point->timestamp_us, gaze_point->position_xy[0], gaze_point->position_xy[1]);
+			else
+				printf ("Gaze point: %g INVALID", gaze_point->position_xy);
+		}, 0);
 
+		if (error != TOBII_ERROR_NO_ERROR)
+		{
+			printf("Failed to subscribe to gaze stream.\n");
+			tobii_device_destroy(device);
+			tobii_api_destroy(api);
+			return 1;
+		}
+
+		while (true) {
+
+		}
+
+
+
+
+		error = tobii_device_destroy(device);
+		assert(error == TOBII_ERROR_NO_ERROR);
+
+		error = tobii_api_destroy(api);
+		assert(error == TOBII_ERROR_NO_ERROR);
+		return 0;
+	}
